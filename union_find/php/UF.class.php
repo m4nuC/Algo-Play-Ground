0edc810a6208773521763f3233b36e49306ec186
m4nuC <m4nu@hotmail.fr>
2013-04-07 00:33:35 +0200

    Adds a sibbling metod to find other gourps memebers

--- union_find/php/UF.class.php
+++ union_find/php/UF.class.php
@@ -7,7 +7,7 @@ class UF {
 	public function __construct( $N )
 	{
 		// We store the Number as Index not as Value so that we can easily access them
-		for ($i = 0; $i < $N; $i++ )
+		for ($i = 1; $i <= $N; $i++ )
 		{
 			$this->objects[$i] = $i;
 		}
@@ -20,25 +20,54 @@ class UF {
 		$this->dpo();
 
 		// If this objects have the same value them they are connected we can return 
-		if ($objects[$a] == $objects[$b]) return;
+		if ($this->objects[$a] == $this->objects[$b]) return;
 
-		$groupId = $objects[$a];
-		$objects[$b] = $groupId;
+		// Get the Group Id of the first object
+		$groupId = $this->objects[$a];
 
+		// Check if the second object is part of a group	
+		$siblings = $this->siblings($b);
+
+		if ($siblings)
+		{
+			foreach ( $siblings as $obj => $value )
+			{
+				$this->objects[$value] = $groupId;
+			}
+		}
+		
 		$this->dpo();
+	}
+
+	public function siblings($obj) 
+	{
+		// Get the group ID
+		$groupId = $this->objects[$obj];
+
+		// Search the array
+		$results = array_keys($this->objects, $groupId, FALSE);
+		return $results;
 	}
 
 	public function connected($a, $b)
 	{
-		//
+		// 
 	}
 
 	private function dpo()
 	{
-		var_dump($his->objects);
+		print("\n");
+		var_dump($this->objects);
 	}
 
 }
 
 
-$UF = new UF(10)
+$UF = new UF(10);
+$UF->union(1,2);
+$UF->union(5,2);
+$UF->union(6,2);
+$UF->union(2,3);
+$UF->union(10,6);
+
+
